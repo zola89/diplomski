18c0e8ba3e795b54a4bc078488a255542377403e
zola89 <zola89@gmail.com>
2014-01-03 14:43:30 +0100

    prvi komit

---
+++ src/Polinom.java
@@ -0,0 +1,88 @@
+import org.jscience.mathematics.function.Polynomial;
+import org.jscience.mathematics.function.RationalFunction;
+import org.jscience.mathematics.function.Term;
+import org.jscience.mathematics.function.Variable;
+import org.jscience.mathematics.number.Complex;
+
+
+public class Polinom {
+	 
+	private static final int ITER = 999;
+    private static double epsilon = 1E-15;
+	    
+	    
+	private Polinom() {}
+	
+    public static Polynomial<Complex> create(Complex... a) {
+        Variable<Complex> x = new Variable.Local<Complex>("x");
+        Polynomial<Complex> px = Polynomial.valueOf(Complex.ZERO, x);
+        for (int i = 0, e = a.length - 1; i < a.length; i++, e--) {
+            px = px.plus(Polynomial.valueOf(a[i], Term.valueOf(x, e)));
+        }
+        return px;
+    }
+    
+    public static Complex eval(Complex[] ca, Complex x) {
+        Complex result = ca[0];
+        for (int i = 1; i < ca.length; i++) {
+           result = result.times(x).plus(ca[i]);
+        }
+        return result;
+    }
+    
+//    public static Complex koef(RationalFunction<Complex> c){
+//    	Variable<Complex> x = new Variable.Local<Complex>("x");
+//    	Complex temp = Complex.ONE;
+//    	temp = (c.getDividend().)/
+//    	return 0;
+//    }
+//    
+//    public static Polynomial<Complex>diff(Polynomial<Complex> px){
+//        Variable<Complex> x = new Variable.Local<Complex>("x");
+//    	Polynomial<Complex> dx = px.differentiate(x);
+//        return dx;
+//    }
+    
+    public static Complex[] roots(Complex[] ca){
+    	Complex[] a0 = new Complex[ca.length - 1];
+        Complex[] a1 = new Complex[ca.length - 1];
+
+        // Divide by leading coefficient if not monic
+        Complex leading = ca[0];
+        if (!ca[0].equals(Complex.ONE)) {
+            for (int i = 0; i < ca.length; i++) {
+                ca[i] = ca[i].divide(leading);
+            }
+        }
+
+        // Initialize a0
+        Complex result = Complex.valueOf(0.4, 0.9);
+        a0[0] = Complex.ONE;
+        for (int i = 1; i < a0.length; i++) {
+            a0[i] = a0[i - 1].times(result);
+        }
+
+    	double delta =0;
+    	//iteracije
+    	for (int k = 1; k < ITER; k++) {
+			delta=0;
+			for (int j =0; j < a1.length; j++) {
+				result = Complex.ONE;
+	    		for (int i = 0; i < a1.length; i++) {
+					if (i!=j)result= a0[j].minus(a0[i]).times(result) ;//Q
+					
+				}
+	    		a1[j]= Polinom.eval(ca, a0[j]).times(-1.0).divide(result);//deltaZj
+	    		if( a1[j].isLargerThan(Complex.ONE.times(delta))) 
+	    			delta = a1[j].magnitude();//if abs(Zj)>delta; delta= abs(Zj);
+			}
+			for (int i = 0; i < a1.length; i++) {
+				a0[i]=a0[i].plus(a1[i]);//Zj +=deltaZj
+			}
+			if (delta <= epsilon) break;
+		}
+    	
+    	return a0;
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,170 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import org.jscience.mathematics.function.Polynomial;
+import org.jscience.mathematics.function.RationalFunction;
+import org.jscience.mathematics.function.Term;
+import org.jscience.mathematics.function.Variable;
+import org.jscience.mathematics.number.Complex;
+public class Test {
+    private static double epsilon = 1E-14;
+    private static final DecimalFormat form =
+            new DecimalFormat(" 0.00000000000000E0;-0.00000000000000E0");
+    /**
+	 * @param args
+	 */
+	
+	private Test(){}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testiranje(5,1,-1,0,0,0,2,1,2);
+		
+//		testiranje(1,-2,1);
+//		
+//		testiranje(1,0,0,-1);
+	}
+	private static void tesiranje(Complex... c){
+		Polynomial<Complex> px = Polinom.create(c);
+        System.out.println("Polinom: " + px);
+        Complex[] roots = Polinom.roots(c);
+        for (Complex complex : roots) {
+			System.out.println(complex.toText());
+		}
+	}
+	
+	private static void testiranje(double... a){
+		 //da baci izuzetak ako a[0] i a[a[0]+1] nisu celi brojevi
+		long duzina = Math.round(a[0]);
+		Complex[] niz = new Complex[(int) duzina];
+		Complex[] niz1 = new Complex[(int) a[(int)duzina+1]];
+		for (int i = 0; i < duzina; i++) {//imenilac
+			niz[i] = Complex.valueOf(a[i+1], 0);
+			
+		}
+		for (int i = 0; i < a.length-((int)duzina+2); i++) {//brojilac
+			niz1[i] = Complex.valueOf(a[i+(int)duzina+2], 0);
+			
+		}
+		Polynomial<Complex> px = Polinom.create(niz);
+
+		Polynomial<Complex> px1 = Polinom.create(niz1);
+		RationalFunction<Complex> rat = RationalFunction.valueOf(px1, px);
+		
+        
+		System.out.println("Racionalna funkcija:" + rat);
+        System.out.println("Polinom: " + px);
+        Complex[] roots = Polinom.roots(niz);
+        int [] b = duplicates(roots);
+		System.out.println("duplikati:" );
+		for (int i = 0; i < b.length; i++) {
+			System.out.println(b[i]);
+	        
+		}
+        validate(niz, roots);
+		System.out.println("nediferenciran:"+px);
+		//rat=rat.getDivisor().differentiate(px.getVariable("x"));
+		System.out.println("diferenciran:"+px);
+		RationalFunction<Complex> temp = rat;
+		  Variable<Complex> x = new Variable.Local<Complex>("x");
+	        Polynomial<Complex> t = Polynomial.valueOf(Complex.ONE, x);
+		for (int i = 0; i < b.length; i++) {
+			if (b[i]>1){
+				t= t.minus(Polynomial.valueOf(roots[i], Term.valueOf(x, 0 )));
+				t=t.pow(b[i]);
+				temp =RationalFunction.valueOf(temp.getDividend().times(t), temp.getDivisor());
+				for (int j = b[i]; j < 0; j--) {
+					temp.differentiate(temp.getVariable("x"));//dupla for petlja za svaku nulu
+				}
+				temp.evaluate(roots[i]);//dodaj promeljivu
+				//i+=b[i];
+				
+			}
+		}
+		
+		rat= RationalFunction.valueOf(px1,rat.getDivisor().differentiate(px.getVariable("x")));
+        for (int i = 0; i < roots.length; i++) {
+        	Complex ck =Complex.ONE;
+        	
+        	ck=rat.getDividend().evaluate(roots[i]).divide(rat.getDivisor().evaluate(roots[i]));
+        	System.out.println("parametar:"+ck);
+        	
+		}
+//        for (Complex complex : roots) {
+//			System.out.println(complex.toText());
+//		}
+	}
+	
+	
+	
+    private static void validate(Complex[] ca, Complex... r) {
+        double max = 0.0;
+        Arrays.sort(r);
+        int ix = 0;
+        while (ix < r.length) {
+            Complex error = Polinom.
+                eval(ca, r[ix]).minus(Complex.ZERO);
+            max = Math.max(max, error.magnitude());
+            double re = r[ix].getReal();
+            double im = r[ix].getImaginary();
+            if (Math.abs(re) < epsilon) re = 0;
+            if (Math.abs(im) < epsilon) im = 0;
+            if (im == 0) {
+                System.out.println("Real: " + form.format(re));
+            } else {
+                if (ix + 1 < r.length && conjugate(r[ix], r[ix + 1])) {
+                    System.out.println("Comp: " + form.format(re)
+                        + " +-" + form.format(Math.abs(im)) + "i");
+                    ix++;
+                } else {
+                    System.out.println("Comp: " + form.format(re)
+                        + (im < 0 ? " -" : " +")
+                        + form.format(Math.abs(im)) + "i");
+                }
+            }
+            ix++;
+        }
+        System.out.println("Error: "
+            + (max < epsilon ? "< " + epsilon : form.format(max))
+            + "\n");
+    }
+    
+    
+    private static boolean conjugate(Complex a, Complex b) {
+        double dr = a.getReal() - b.getReal();
+        double di = Math.abs(a.getImaginary()) - Math.abs(b.getImaginary());
+        return Math.abs(dr) < epsilon && Math.abs(di) < epsilon;
+    }
+
+    private static boolean equalNumbers(Complex a, Complex b) {
+        double dr = a.getReal() - b.getReal();
+        double di = a.getImaginary() - b.getImaginary();
+        return Math.abs(dr) < epsilon && Math.abs(di) < epsilon;
+    }
+    
+    private static int[] duplicates(Complex[] c){
+		int t=1;
+		int[] b = new int[c.length];
+		b[0]=1;
+		for (int i=1; i<c.length;i++) {
+			 b[i]=1;
+			 if (equalNumbers(c[i], c[i-1])){ b[i]=++t;}
+			 else {
+				 	 
+				 	t=1;
+			 }
+			 
+		}
+		for (int i = b.length-1; i >0;i--) {
+			if(b[i]>1){
+				Arrays.fill(b, i-b[i]+1, i, b[i]);
+				i-=b[i]-1;
+			}
+			
+		}
+    	
+    	return b;
+    	
+    }
+
+}
